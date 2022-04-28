b9fe49a704bed7445aa292e015a59c66ccd7e765
jordnkr <jordnkr408@gmail.com>
2022-04-28 14:14:47 -0500

    update index.js

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const core = require('@actions/core');
 const AxeBuilder = require('@axe-core/webdriverjs');
 const WebDriver = require('selenium-webdriver');
 
-const driver = new WebDriver.Builder().forBrowser('chrome').build();
-const testUrl = core.getInput('url');
+(async function example() {
+  let driver = await new WebDriver.Builder().forBrowser('chrome').build();
+  const testUrl = core.getInput('url');
 
-driver.get(testUrl).then(() => {
-  new AxeBuilder(driver).analyze((err, results) => {
-    if (err) {
-      // Handle error somehow
-    }
-    console.log(results);
+  driver.get(testUrl).then(() => {
+    new AxeBuilder(driver).analyze((err, results) => {
+      if (err) {
+        // Handle error somehow
+      }
+      console.log(results);
+    });
   });
-});
+
+  /*try {
+    await driver.get('http://www.google.com/ncr');
+    await driver.findElement(By.name('q'));.sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    await driver.quit();
+  }*/
+})();
