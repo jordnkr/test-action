ce45225b6363c1d9cccbbdcfe689f8d576180091
jordnkr <jordnkr408@gmail.com>
2022-04-28 15:40:46 -0500

    test again

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const core = require('@actions/core');
 const AxeBuilder = require('@axe-core/webdriverjs');
 const WebDriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 
 (async function example() {
-  var chromeCapabilities = WebDriver.Capabilities.chrome();
-  
-  var chromeOptions = {
-      'args': ['--headless', '--no-sandbox', '--remote-debugging-port=9222']
-  };
-  chromeCapabilities.set('chromeOptions', chromeOptions);
-  
-  let driver = await new WebDriver.Builder().withCapabilities(chromeCapabilities).build();
+  var driver = await new WebDriver.Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('--headless', '--no-sandbox')).build();
   /*const testUrl = core.getInput('url');
 
   driver.get(testUrl).then(() => {
