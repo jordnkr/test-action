c60e2e799c5b94ee3802d4ce27c14db2b98e0cd8
jordnkr <jordnkr408@gmail.com>
2022-04-30 13:37:41 -0500

    updates

--- index.js
+++ index.js
@@ -5,19 +5,18 @@ const chrome = require('selenium-webdriver/chrome');
 
 (async function example() {
   let driver;
-  try {
-    driver = await new WebDriver.Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('--headless')).build();
-    const testUrl = core.getInput('url');
-    await driver.get(testUrl);
-    new AxeBuilder(driver).analyze((err, results) => {
-      if (err) {
-        // Handle error
-      }
-      console.log(results);
-    });
-  } catch (error) {
-    console.error(error);
-  } finally {
-    await driver.quit();
-  }
+  driver = await new WebDriver.Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('--headless')).build();
+  
+  const testUrl = core.getInput('url');
+  await driver.get(testUrl);
+  
+  const builder = new AxeBuilder(driver);
+  
+  builder.analyze((err, results) => {
+    if (err) {
+      // Handle error
+    }
+    console.log(results);
+  });
+  await driver.quit();
 })();
