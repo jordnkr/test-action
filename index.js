2bcd1f298e4e6977b83f59f02e0f66a4b5ff13e3
jordnkr <jordnkr408@gmail.com>
2022-04-30 13:42:50 -0500

    rollbaack

--- index.js
+++ index.js
@@ -8,15 +8,12 @@ const chrome = require('selenium-webdriver/chrome');
   driver = await new WebDriver.Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('--headless')).build();
   
   const testUrl = core.getInput('url');
-  await driver.get(testUrl);
-  
-  const builder = new AxeBuilder(driver);
-  
-  builder.analyze((err, results) => {
-    if (err) {
-      // Handle error
-    }
-    console.log(results);
+  driver.get(testUrl).then(() => {
+    new AxeBuilder(driver).analyze((err, results) => {
+      if (err) {
+        // Handle error somehow
+      }
+      console.log(results);
+    });
   });
-  await driver.quit();
 })();
